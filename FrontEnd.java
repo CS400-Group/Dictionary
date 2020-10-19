9bc67dc811edb6ecaa08bc08ddda0b82da07ad66
gabebohlmann <71666005+gabebohlmann@users.noreply.github.com>
2020-10-19 16:22:17 -0500

    FrontEnd v1.4

--- FrontEnd.java
+++ FrontEnd.java
@@ -1,9 +1,8 @@
-import java.util.Dictionary;
 import java.util.Scanner;
 
 public class FrontEnd {
 	
-	Dictionary dictionary = new Dictionary();
+	static Dictionary dictionary = new Dictionary();
 	private static Scanner in = new Scanner(System.in);
 	private static String input = "";
 	
@@ -12,17 +11,21 @@ public class FrontEnd {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
 		do {
 			menu();
-			System.out.print("Enter [m] to perform a more actions or [q] to quit: ");
+			if (input.equalsIgnoreCase("q")) {
+				break;
+			}
+			System.out.print("\nEnter [m] to perform more actions or [q] to quit: ");
 			input = in.nextLine();
-			while (!input.equalsIgnoreCase("m") || !input.equalsIgnoreCase("q")) {
+			while (!input.equals("m") && !input.equalsIgnoreCase("q")) {
 				System.out.print("\nInput is invalid, please try again: ");
 				input = in.nextLine();
 			}
+			
 			if (input.equalsIgnoreCase("q")) {
 				if(isQuitValid()) {
+					System.out.println("Thank you for using Dictionary");
 					break;
 				}
 			}
@@ -40,7 +43,6 @@ public class FrontEnd {
 		System.out.print("New Dictionary Created: \n"
 				+ "Please input a letter corresponding to one of the following actions. \n"
 				+ "   [a] Add word \n"
-				+ "   [r] Remove word  \n"
 				+ "   [s] Search for word \n"
 				+ "   [p] Print dictionary contents \n"
 				+ "   [c] Clear dictionary contents \n"
@@ -48,29 +50,33 @@ public class FrontEnd {
 				+ "Enter input here: ");	
 		input = in.nextLine();
 		while (!isValidInput(input)) {
-			System.out.print("\nInput is invalid, please try again: ");
+			System.out.print("Input is invalid, please try again: ");
 			input = in.nextLine();
 		}		
 		switch (input) {
 			case "a": 
 				addWord();
 				break;
-			case "r": 
-				removeWord();
 			case "s":
 				search();
+				break;
 			case "p":
 				print();
+				break;
 			case "c":
 				clear();
+				break;
 			case "q":
 				if (isQuitValid()) {
-					System.out.println("Thank you for using Dictionary")
+					System.out.println("Thank you for using Dictionary");
+					break;
 				} else {
+					System.out.println("Quitting has been cancelled. Returning to menu.");
 					break;
 				}
 		}
 	}
+	
 	
 	/**
 	 * Determines if an input is valid
@@ -78,7 +84,7 @@ public class FrontEnd {
 	 * @return true if input is confirmed to be valid
 	 */
 	public static boolean isValidInput(String input) {
-		String[] validInputs = {"a", "r", "s", "p", "c", "q"};
+		String[] validInputs = {"a", "s", "p", "c", "q"};
 		for (String values : validInputs) {
 			if (values.equalsIgnoreCase(input)) {
 				return true;
@@ -92,33 +98,32 @@ public class FrontEnd {
 	 * @return true if user confirms quitting
 	 */
 	public static boolean isQuitValid() {
-		System.out.println("Are you sure you want to quit? \n"
+		System.out.print("\nAre you sure you want to quit? \n"
 				+ "Enter [y] for yes or [n] for no: ");
-		input = in.nextLine();
-		while (!input.equalsIgnoreCase("y") || !input.equalsIgnoreCase("n")) {
+		String verify = in.nextLine();
+		while (!verify.equalsIgnoreCase("y") && !verify.equalsIgnoreCase("n")) {
 			System.out.print("\nInput is invalid, please try again: ");
-			input = in.nextLine();
+			verify = in.nextLine();
 		}
-		if (input.equalsIgnoreCase("y")) {
+		if (verify.equalsIgnoreCase("y")) {
 			return true; 
-		} else if (input.equalsIgnoreCase("n")) {
+		} else {
 			return false;
 		}
-		
 	}
 	
 	/**
 	 * Constructs a word object based on user input and adds the word to the dictionary
 	 */
-	public void addWord() {
-		System.out.println("Adding a word to the dictibonary");
-		System.out.println("Enter the word: ");
+	public static void addWord() {
+		System.out.println("\nAdding a word to the dictionary");
+		System.out.print("Enter the word: ");
 		String wordStr = in.nextLine();
-		System.out.println("Enter the word's defintion: ");
+		System.out.print("Enter the word's defintion: ");
 		String definition = in.nextLine();
-		System.out.println("Enter the word's origin: ");
+		System.out.print("Enter the word's origin: ");
 		String origin = in.nextLine();
-		System.out.println("Enter the word's synonyms (make sure to seperate synonyms by a space):");
+		System.out.print("Enter the word's synonyms (make sure to seperate synonyms by a space): ");
 		String synonymsStr  = in.nextLine();
 		String[] synonyms = synonymsStr.split(" ");
 		Word word = new Word(wordStr, definition, origin, synonyms);
@@ -126,43 +131,43 @@ public class FrontEnd {
 	}
 	
 	/**
-	 * Finds and removes a user specified word from the dictionary
-	 */
-	public void removeWord() {
-		System.out.println("Removing a word from dictionary "); 
-		System.out.println("Enter word to be removed: ");
-		String wordStr = in.nextLine();
-		Word word = dictionary.get(wordStr);
-		dictionary.remove(word);
-		System.out.println("Word succuesfully removed");
-	}
-	/**
 	 * Finds a specified word in the dictionary
 	 */
-	public void search() {
+	public static void search() {
 		System.out.println("Searching for word in dictionary: ");
-		System.out.println("Enter word to search for: ");
+		System.out.print("Enter word to search for: ");
 		String wordStr = in.nextLine();
-		System.out.println(dictionary.get(wordStr).toString());
+		try {
+			if (dictionary.get(wordStr) != null) {
+				System.out.println("\nWord Found!, search results below: \n");
+			}
+			System.out.println(dictionary.get(wordStr).wordInfo());
+		} catch (NullPointerException e) {
+			System.out.println("Word is not in dictionary.");
+		}
 	}
 	
 	/**
 	 * Prints the dictionary's contents
 	 */
-	public void print() {
-		System.out.print("Printing the dictionary's contents");
-		dictionary.print();
+	public static void print() {
+		System.out.println("Printing the dictionary's contents:");
+		if (dictionary.size() == 0) {
+			System.out.println("The dictionary is empty");
+		} else {
+			dictionary.print();
+		}
 	}
 	
 	/**
 	 * Clears the dictionary of all words
 	 */
-	public void clear() {
-		System.out.print("Clearig dictionary's contents");
-		System.out.println("Are you sure you want to clear the dictionary? \n"
+	public static void clear() {
+		System.out.println("Clearig dictionary's contents");
+		System.out.print("Are you sure you want to clear the dictionary? \n"
 				+ "Enter [y] for yes or [n] for no: ");
 		input = in.nextLine();
-		while (!input.equalsIgnoreCase("y") || !input.equalsIgnoreCase("n")) {
+		while (!input.equalsIgnoreCase("y") && !input.equalsIgnoreCase("n")) {
 			System.out.print("\nInput is invalid, please try again: ");
 			input = in.nextLine();
 		}
@@ -174,8 +179,5 @@ public class FrontEnd {
 		}
 		
 	}
-	
-	
-	
 	
 }
